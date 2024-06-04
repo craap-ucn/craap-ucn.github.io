83fdb85c9c099bf5933aabc33fab8c2e9cf22df0
ExequielGL <exequiel.gonzales@gmail.com>
2024-06-04 19:31:23 -0400

    feat: Progress on student page

--- assets/js/tailwind.js
+++ assets/js/tailwind.js
@@ -4,7 +4,8 @@ tailwind.config = {
             colors: {
                 'blue-ucn-header': '#003057',
                 'blue-ucn-footer': '#23405B',
-
+                'gray-hard-custom': '#808080',
+                'gray-light-custom': '#E6E6E6',
             },
             fontFamily: {
                 'arial-regular': ['Arial', 'sans-serif'],
